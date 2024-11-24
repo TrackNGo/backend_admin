104e727644a44b1839a607c923c19447d26bd0a4
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-24 23:24:08 +0530

    feat: Create LostAndFoundItem model for managing lost and found items

---
+++ src/models/LostNfoundModel.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from 'mongoose'
+import lostNfoundDetail from '../interfaces/lostandFound'
+
+const lostAndFoundItemSchema = new Schema<lostNfoundDetail>(
+    {
+        description: {
+          type: String,
+          required: true,
+        },
+        location: {
+          type: String,
+          required: true,
+        },
+        date: {
+          type: Date,
+          required: true,
+          default: Date.now,
+        },
+        busRoute: {
+          type: String,
+          required: true,
+        },
+        contactDetails: {
+          type: String,
+          required: true,
+        },
+        status: {
+          type: String,
+          enum: ['lost', 'found'],
+          required: true,
+        },
+      },
+      { timestamps: true }
+    );
+
+const LostAndFoundItemModel = mongoose.model('LostAndFoundItem', lostAndFoundItemSchema )
+
+export default LostAndFoundItemModel

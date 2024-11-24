a8b0b9c78313cb544d17e0e77d8dfea4e47840cc
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-24 22:02:47 +0530

    feat: Integrate user and auth routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import connectDB from './config/dbConfig'
 import dotenv from 'dotenv'
 
+import userRoutes from './routes/UserRoute'
+import authRoutes from './routes/AuthRoute'
+
 dotenv.config()
 const app = express()
 app.use(express.json())
@@ -9,6 +12,11 @@ app.use(express.json())
 const PORT= process.env.PORT || 3000
 
 connectDB()
+
+app.use('/api-user', authRoutes)
+app.use('/api-user', userRoutes)
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)

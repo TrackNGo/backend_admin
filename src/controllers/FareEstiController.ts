1268f25f02b5feafbb6a91346f8b46336b154697
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-15 22:46:56 +0530

    feat: add getFareEstimates function to retrieve fare estimates

--- src/controllers/FareEstiController.ts
+++ src/controllers/FareEstiController.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from "express";
 import FareEstimate from "../models/FareEstimateModel";
 
+
+export const getFareEstimates = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const fareEstimates = await FareEstimate.find();
+
+        if (!fareEstimates || fareEstimates.length === 0) {
+            return res.status(404).json({ message: "No fare estimates found" })
+        }
+
+        res.status(200).json({ fareEstimates });
+    }
+    catch (error: any) {
+        console.error("Error fetching fare estimates:", error);
+        res.status(500).json({ message: "An error occurred while fetching fare estimates", error: error.message || "Internal Server Error" });
+    }
+}
+
 // Add a fare between two stops on the bus route
 export const addFareToRoute = async (req: Request, res: Response): Promise<any> => {
     const { routeNumber, busType, startStop, endStop, estimatedFare } = req.body;

8791cffef14c7ebe680c449588939639a6dcf931
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-15 22:47:42 +0530

    feat: update fare routes to include specific endpoints for add, delete, and update operations

--- src/routes/FareRoute.ts
+++ src/routes/FareRoute.ts
@@ -1,15 +1,17 @@
 import express from 'express';
-import {addFareToRoute,deleteFareForStop,updateFareForStop} from '../controllers/FareEstiController';
+import { addFareToRoute, deleteFareForStop, getFareEstimates, updateFareForStop } from '../controllers/FareEstiController';
 
 const router = express.Router();
 
 // Route to add a fare between two stops on the bus route
-router.post('/', addFareToRoute);
+router.post('/add', addFareToRoute);
+
+router.get('/view', getFareEstimates)
 
 // Route to delete fare for a specific stop on the bus route
-router.delete('/', deleteFareForStop);
+router.delete('/delete', deleteFareForStop);
 
 // Route to update fare for a specific stop on the bus route
-router.put('/', updateFareForStop);
+router.put('/update', updateFareForStop);
 
 export default router;

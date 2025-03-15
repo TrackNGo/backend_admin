c49f504c31534e8c0635f137ad24f4ed5446d532
Udara Nalawansa <udarakavishka13@gmail.com>
2025-03-15 21:09:12 +0530

    feat: add routeNumber and busType to FareEstimate interface

--- src/interfaces/fareEstimate.ts
+++ src/interfaces/fareEstimate.ts
@@ -2,9 +2,11 @@ import { Document } from "mongoose";
 
 export interface FareEstimate extends Document {
     busNumber: string;
+    routeNumber: string;  
     startStop: string;
     endStop: string;
     estimatedFare: number;
+    busType: 'Normal' | 'Semi-Luxury' | 'Luxury'; 
 }
 
 export default FareEstimate;

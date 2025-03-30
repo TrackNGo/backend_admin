9678ee18654c43fe8623d6fc0330df698d11a197
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-03-30 14:39:58 +0530

    feat: create conductor auth login feature

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -65,6 +65,56 @@ export const Login: RequestHandler = async (req: Request, res: Response) => {
     }
 };
 
+export const LoginConductor: RequestHandler = async (req: Request, res: Response) => {
+    const { loginIdentifier, password } = req.body;
+
+    try {
+
+        if (!loginIdentifier || !password) {
+            res.status(400).json({ message: "Login identifier and password are required" });
+            return;
+        }
+
+        // Find the user by username or mobile
+        const user = await UserModel.findOne({busNumber: loginIdentifier });
+
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+            return;
+        }
+
+        // Validate the password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({ message: "Invalid credentials" });
+            return;
+        }
+
+        // Check if the user account is active
+        // if (!user.isActive) {
+        //     res.status(403).json({ message: "User account is inactive" });
+        //     return;
+        // }
+
+        // Generate a JWT token
+        const token = generateJwt(user);
+
+        // Send response
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                busNumber: user.busNumber,
+                username: user.username,
+                accType: user.accType,
+            },
+        });
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ message: "An error occurred during login" })
+    }
+};
+
 export const Logout: RequestHandler = async (req: Request, res: Response) => {
     try {
         // Invalidate the JWT token by clearing it client-side
--- src/routes/AuthRoute.ts
+++ src/routes/AuthRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { Login, Logout } from '../controllers/AuthController'
+import { Login, Logout, LoginConductor} from '../controllers/AuthController'
 
 const router=express.Router()
 
 router.post('/login',Login)
+router.post('/login-conductor',LoginConductor)
 router.post('/logout', Logout)
 
 export default router
